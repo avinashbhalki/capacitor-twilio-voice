ff2982d3a00c4ac6ddba327bcaa604867d53f784
Avinash Bhalki <avinash.bhalki@quantummaterialscorp.com>
2025-12-26 10:07:12 +0530

    plugin deleted

--- dist/esm/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { TwilioVoicePlugin } from './definitions';
-declare const TwilioVoice: TwilioVoicePlugin;
-export * from './definitions';
-export { TwilioVoice };
-//# sourceMappingURL=index.d.ts.map
--- dist/esm/index.js
+++
@@ -1,6 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const TwilioVoice = registerPlugin('TwilioVoice', {
-    web: () => import('./web').then(m => new m.TwilioVoiceWeb()),
-});
-export * from './definitions';
-export { TwilioVoice };
