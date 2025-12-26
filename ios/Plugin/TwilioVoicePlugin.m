5cdd57d655059eb40c9e7170f6a2b652b741b4c2
Avinash Rao Bhalki <avinash207@gmail.com>
2025-12-26 23:05:29 +0530

    Update TwilioVoicePlugin.m

--- ios/Plugin/TwilioVoicePlugin.m
+++ ios/Plugin/TwilioVoicePlugin.m
@@ -1,8 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Define the plugin with the JS name and methods
-// This makes the 'call' method visible to the Capacitor bridge
+// This registers the plugin and the 'call' method with Capacitor
 CAP_PLUGIN(TwilioVoicePlugin, "TwilioVoice",
            CAP_PLUGIN_METHOD(call, CAPPluginReturnPromise);
 )
