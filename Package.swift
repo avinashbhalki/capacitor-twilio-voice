820e8d3c17478c0c773af25e5f5c40e60b0e5ef2
Avinash Bhalki <avinash.bhalki@quantummaterialscorp.com>
2025-12-30 14:29:19 +0530

    iOS plugin updated

--- Package.swift
+++ Package.swift
@@ -1,35 +1,25 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-// DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
 let package = Package(
-    name: "CapApp-SPM",
-    platforms: [.iOS(.v15)],
+    name: "AvinashbhalkiCapacitorTwilioVoice",
+    platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "CapApp-SPM",
-            targets: ["CapApp-SPM"])
+            name: "AvinashbhalkiCapacitorTwilioVoice",
+            targets: ["AvinashbhalkiCapacitorTwilioVoice"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
-        .package(name: "AvinashbhalkiCapacitorTwilioVoice", path: "../../../node_modules/@avinashbhalki/capacitor-twilio-voice"),
-        .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
-        .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics"),
-        .package(name: "CapacitorKeyboard", path: "../../../node_modules/@capacitor/keyboard"),
-        .package(name: "CapacitorStatusBar", path: "../../../node_modules/@capacitor/status-bar")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
+        .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.9.0")
     ],
     targets: [
         .target(
-            name: "CapApp-SPM",
+            name: "AvinashbhalkiCapacitorTwilioVoice",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "AvinashbhalkiCapacitorTwilioVoice", package: "AvinashbhalkiCapacitorTwilioVoice"),
-                .product(name: "CapacitorApp", package: "CapacitorApp"),
-                .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
-                .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
-                .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar")
-            ]
-        )
+                .product(name: "TwilioVoice", package: "twilio-voice-ios")
+            ],
+            path: "ios/Plugin")
     ]
 )
