a5534af635feff835e0e79a0da5bedf42af41c87
Avinash Bhalki <avinash.bhalki@quantummaterialscorp.com>
2025-12-30 13:58:51 +0530

    ios Code Unimplemented error fixed

--- ios/Plugin/TwilioVoicePlugin.m
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-// This registers the plugin and the 'call' method with Capacitor
-CAP_PLUGIN(TwilioVoicePlugin, "TwilioVoice",
-           CAP_PLUGIN_METHOD(call, CAPPluginReturnPromise);
-)
